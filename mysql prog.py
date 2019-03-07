0f34905b6e73698ab501eadf9256f712472a2a46
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-03-07 15:04:03 +0100

    Update mysql prog.py

--- mysql prog.py
+++ mysql prog.py
@@ -4,26 +4,63 @@ select * from customer;
 
 
 #livreur
-select customer.name, customer.number, customer.address, command.delay,
-command.idProduct, product.name, product.price_product, command.ChoicePayment,
-Linking_command_product.totalPrice
+select customer.name, customer.number, customer.address,
+command.id_order,
+product.name,product.price_product
+
 from customer
+
 inner join command on command.id_customer = customer.id_customer
 inner join Linking_command_product on Linking_command_product.id_order = command.id_order
-inner join product on command.idProduct = product.id_product;
+inner join product on product.id_product = Linking_command_product.id_product
+
+where command.id_order = 1;
+
+
+select customer.name, customer.number, customer.address,
+command.id_order,
+product.name,product.price_product
+
+from customer
+
+inner join command on command.id_customer = customer.id_customer
+inner join Linking_command_product on Linking_command_product.id_order = command.id_order
+inner join product on product.id_product = Linking_command_product.id_product
+
+where command.id_order = 2;
+
+
 #-----
 
 #serveur addition
 select command.id_order, sum(product.price_product) as prix_command
 from customer
-inner join command on command.id_customer = customer.id_customer
-inner join product on command.idProduct = product.id_product;
 
-select idProduct,
-price_product
-from command
-inner join customer on customer.id_customer = command.id_customer
-inner join product on command.idProduct = product.id_product
+inner join command on command.id_customer = customer.id_customer
+inner join Linking_command_product on Linking_command_product.id_order = command.id_order
+inner join product on product.id_product = Linking_command_product.id_product
+
+where customer.id_customer = 2;
+
+select command.id_order, sum(product.price_product) as prix_command
+from customer
+
+inner join command on command.id_customer = customer.id_customer
+inner join Linking_command_product on Linking_command_product.id_order = command.id_order
+inner join product on product.id_product = Linking_command_product.id_product
+
+where customer.id_customer = 1;
+
+
+
+select customer.name, customer.id_customer,
+Product.Price_product , Product.name, Product.id_product
+from customer
+
+inner join Command as commande on commande.id_customer = customer.id_customer
+inner join Linking_command_product on commande.id_order = Linking_command_product.id_order
+inner join Product on Linking_command_product.id_product = Product.id_product
+
 where customer.id_customer = 1;
 #------
 
@@ -31,20 +68,33 @@ where customer.id_customer = 1;
 
 
 
-#facture--
+#facture detail
 select bill.id_bill, bill.id_order, bill.price, bill.date_order,
 command.id_customer, command.id_store, command.date,
 Linking_command_product.id_product,
 product.price_product
-from bill
-inner join command on command.id_order = bill.id_order
-inner join Linking_command_product on Linking_command_product.id_order = command.id_order
-inner join product on Linking_command_product.id_product = product.id_product;
 
+from bill
+
+inner join command on command.id_order = bill.id_order
+inner join customer on command.id_customer = customer.id_customer
+inner join Linking_command_product on Linking_command_product.id_order = command.id_order
+inner join product on Linking_command_product.id_product = product.id_product
+
+where customer.id_customer = 2;
+
+
+#prix total facture
 select command.id_order, sum(product.price_product) as prix_command
 from customer
 inner join command on command.id_customer = customer.id_customer
-inner join product on command.idProduct = product.id_product;
+inner join Linking_command_product on command.id_order = Linking_command_product.id_order
+inner join product on Linking_command_product.id_product = product.id_product
+where customer.id_customer = 2;
+
+
+
+
 #--------
 
 
@@ -58,7 +108,8 @@ Linking_cart_product.totalPrice
 from customer
 inner join cart on cart.id_customer = customer.id_customer
 inner join Linking_cart_product on Linking_cart_product.id_cart = cart.id_cart
-inner join product on product.id_product = Linking_cart_product.id_product;
+inner join product on product.id_product = Linking_cart_product.id_product
+where customer.id_customer = 2;
 #---------------------
 
 
@@ -121,21 +172,24 @@ inner join customer on customer.id_customer = command.id_customer;
 
 
 #appuie sur l'une des commandes
-select customer.id_customer, command.idProduct, product.name, product.picture
-from command
-inner join customer on customer.id_customer = command.id_customer
-inner join Linking_command_product on Linking_command_product.id_order= command.id_order
-inner join Product on product.id_product = Linking_command_product.id_product
-where customer.id_customer = 2;
-
-
-select customer.id_customer, command.idProduct, product.name, product.picture
-from command
-inner join customer on customer.id_customer = command.id_customer
+select customer.id_customer, command.id_order, product.name, product.picture
+from customer
+inner join command on customer.id_customer = command.id_customer
 inner join Linking_command_product on Linking_command_product.id_order= command.id_order
 inner join Product on product.id_product = Linking_command_product.id_product
 where customer.id_customer = 1;
 
+
+select customer.id_customer, command.id_order, product.name, product.picture
+from customer
+inner join command on customer.id_customer = command.id_customer
+inner join Linking_command_product on Linking_command_product.id_order= command.id_order
+inner join Product on product.id_product = Linking_command_product.id_product
+where customer.id_customer = 1;
+#--------------
+
+
+#category d'un produit ?
 
 
 
